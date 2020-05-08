11c1f9258a1563d970bd6fb5dc16d6a656589f8d
Junth Basnet <junthbasnet@gmail.com>
2020-05-08 14:32:16 +0545

    Footer added

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,21 +8,18 @@ const Footer = () => (
   <Styled.Footer>
     <Container>
       <Styled.Links>
-        <Styled.Link href="/" rel="noreferrer noopener" target="_blank">
+        <Styled.Link href="https://github.com/Junth" rel="noreferrer noopener" target="_blank">
           GitHub
         </Styled.Link>
+        <Styled.Link href="https://twitter.com/JunthBasnet" rel="noreferrer noopener" target="_blank">
+          Twitter
+        </Styled.Link>
         <Styled.Link
-          href="https://github.com/SaimirKapaj/gatsby-markdown-personal-website"
+          href="https://www.linkedin.com/in/junth-basnet-69915b13a/"
           rel="noreferrer noopener"
           target="_blank"
         >
-          Twitter
-        </Styled.Link>
-        <Styled.Link href="/" rel="noreferrer noopener" target="_blank">
-          Behance
-        </Styled.Link>
-        <Styled.Link href="/" rel="noreferrer noopener" target="_blank">
-          <i class="fab fa-linkedin"></i>
+          LinkedIn
         </Styled.Link>
       </Styled.Links>
     </Container>
